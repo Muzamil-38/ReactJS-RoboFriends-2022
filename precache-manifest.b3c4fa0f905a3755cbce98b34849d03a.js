98b0c460911108c9b7611fd38f6af02f5cdab1c8
Muzamil-38 <59355528+Muzamil-38@users.noreply.github.com>
2022-05-02 23:12:43 +0500

    Updates

---
+++ precache-manifest.b3c4fa0f905a3755cbce98b34849d03a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac29238eb23090ff1b0b",
+    "url": "/RoboFriends-ReactApp/static/css/main.ffb26195.chunk.css"
+  },
+  {
+    "revision": "ac29238eb23090ff1b0b",
+    "url": "/RoboFriends-ReactApp/static/js/main.ac29238e.chunk.js"
+  },
+  {
+    "revision": "1291179ad713a3b863ad",
+    "url": "/RoboFriends-ReactApp/static/css/1.5fb31966.chunk.css"
+  },
+  {
+    "revision": "1291179ad713a3b863ad",
+    "url": "/RoboFriends-ReactApp/static/js/1.1291179a.chunk.js"
+  },
+  {
+    "revision": "62b67070d3438f190c69",
+    "url": "/RoboFriends-ReactApp/static/js/runtime~main.62b67070.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriends-ReactApp/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "89220a57d69742437c201cc42626da50",
+    "url": "/RoboFriends-ReactApp/index.html"
+  }
+];
